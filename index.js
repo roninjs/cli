a59a59cf5571c02cb1ac011a8deaa6ff773ca3d6
peter <peter.mckee@gmail.com>
2020-08-24 15:38:01 -0500

    restructure cli to use index.js

--- index.js
+++ index.js
@@ -1,39 +1,4 @@
-const usage = require( './usage.js' )
-const project = require( './cmds/projects' )
+#!/usr/bin/env node
 
-const commands = [ 'help', 'version' ] 
-
-const validCommands = [ 'create', ...commands ]
-
-async function main() {
-  if( process.argv.length < 3 ) {
-    usage.printAndExit()
-  }
-
-  const command = process.argv[2]
-  const args = process.argv.slice( 3, process.argv.length )
-
-  if( !validCommands.includes( command ) ) {
-    console.log( `ronin: '${command}' is not a ronin command.` )
-    console.log( `See 'ronin --help'` )
-    process.exit()
-  }
-
-  try {
-    switch (command) {
-      case 'create':
-        await project.create( args )
-        break;
-    
-      case 'help': 
-      default:
-        usage.printAndExit()
-        break;
-    }
-  } catch( error ) {
-    console.error( `ERROR: ${error.message}` )
-  }
-
-}
-
-module.exports = main
+const main = require( './roninCli.js' )
+main()
